1a1b5d9f11f2e94b056fbde25cdc216b59c9c0b4
rodzerz <ipa23.r.bernats@vtdt.edu.lv>
2026-01-26 13:36:21 +0200

    kip gatavs

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Flight;
 
 use App\Http\Controllers\FlightController;
 
 Route::get('/flights', [FlightController::class, 'index']);
 
 Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/flights', function () {
+    return Flight::all(['latitude', 'longitude',]);
+});
+Route::get('/flights-map', function () {
     return view('welcome');
 });
